c49f5aa6539530e9da09eb46e76273f364df445f
David Pitchford <david.pitchford.projects@gmail.com>
2019-05-02 23:18:35 +0100

    Start with the basics and then add the install command

--- installswagger/entrypoint.sh
+++ installswagger/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo $PWD
 cd ./mobile
+echo $PWD
 mkdir client
-#npm install -g swagger-nodegen-cli
-#swagger-nodegen-cli generate -i ../api/rosa-api.yaml -l javascript -o client/. --additional-properties usePromises=true,useES6=true
+ls
+npm install -g swagger-nodegen-cli
+swagger-nodegen-cli generate -i ../api/rosa-api.yaml -l javascript -o client/. --additional-properties usePromises=true,useES6=true
