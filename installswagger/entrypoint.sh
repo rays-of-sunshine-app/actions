ece2dee2bb1f515d0d1c4d2ad6cc73ec3e5616cc
David Pitchford <david.pitchford.projects@gmail.com>
2019-05-03 14:00:56 +0100

    Switch to java version

--- installswagger/entrypoint.sh
+++ installswagger/entrypoint.sh
@@ -27,12 +27,12 @@ mkdir client
 echo  "OUTPUT DIRECTORY: " `ls -lrta`
 
 echo "RUNNING: curl for swagger-codegen-cli.2.4.4.jar"
-#wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.4/swagger-codegen-cli-2.4.4.jar -O ../swagger-codegen-cli.jar
-curl http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.4/swagger-codegen-cli-2.4.4.jar --output ./swagger-codegen-cli.jar
+#wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.4/swagger-codegen-cli-2.4.4.jar -O ../swagger-codegen-cli-2.4.4.jar
+curl http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.4/swagger-codegen-cli-2.4.4.jar --output ./swagger-codegen-cli-2.4.4.jar
 
 echo  "OUTPUT DIRECTORY: " `ls -lrta`
 
-chmod 777 swagger-codegen-cli.jar
+chmod 777 swagger-codegen-cli-2.4.4.jar
 
 
 echo "RUNNING: java -jar swagger-codegen-cli-2.4.4.jar generate -i ../api/rosa-api.yaml -l javascript -o client/. --additional-properties usePromises=true,useES6=true"
